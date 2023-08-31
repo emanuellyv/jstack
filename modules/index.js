542140c376e5da669f097864d75160bfeb2d1b49
Emanuelly Valenga <emanuelly.valenga@gmail.com>
2023-08-30 21:59:25 -0300

    Simples explicação sobre módulos no NodeJS

---
+++ modules/index.js
@@ -0,0 +1,6 @@
+const modulo = require('./printName');
+
+modulo.printName(`Emanuelly ${modulo.lastName}`);
+
+const os = require('os');
+console.log(os.type());
---
+++ modules/printName.js
@@ -0,0 +1,7 @@
+exports.printName = (name) => {
+    console.log(name);
+}
+
+exports.lastName = 'Valenga';
+
+// module.exports = { printName, lastName };
